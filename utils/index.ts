c48a0d0b74edc2311c06a8f12319e47e3daac3a6
Vinayak <vinayakcpa@gmail.com>
2023-07-01 16:38:27 -0400

    added custom filters

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
 import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars(filter: FilterProps) {
+export async function fetchCars(filters: FilterProps) {
   const { manufacturer, year, model, limit, fuel } = filters;
 
   const headers = {
@@ -48,4 +48,13 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   url.searchParams.append("angle", `${angle}`);
 
   return `${url}`;
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+  return newPathname;
 };
